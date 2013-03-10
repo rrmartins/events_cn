a27d579fbc9fdb3ddb27f7549cb8e55d240fd1a2
Rodrigo Martins <rodrigo@rrmartins.com>
2013-03-10 13:06:24 -0300

    files of configurations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,27 +1,15 @@
 EventsVn::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-
-  # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-
-  # Do not eager load code on boot.
   config.eager_load = false
-
-  # Show full error reports and disable caching.
+  config.whiny_nils = true
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
-
-  # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
-  # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
-
-  # Raise an error on page load if there are pending migrations
+  config.action_dispatch.best_standards_support = :builtin
+  config.active_record.mass_assignment_sanitizer = :strict
+  config.active_record.auto_explain_threshold_in_seconds = 0.5
   config.active_record.migration_error = :page_load
-
-  # Debug mode disables concatenation and preprocessing of assets.
+  config.assets.compress = false
   config.assets.debug = true
 end
